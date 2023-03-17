68b73cc2f289dbc736013f3774f37007af7460d7
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-17 11:25:26 -0700

    print all base 10 digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - print all base 10 digits
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	while (i < 10)
+	{
+		printf("%i", i);
+		i++;
+	}
+	printf("\n");
+	return (0);
+}
