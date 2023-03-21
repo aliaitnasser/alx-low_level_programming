ba48ca239820ef8fbf142679f6e2b8060f9c79c2
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-21 15:01:10 -0700

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ *_abs - return the absulute value of an int
+ *@n: int to convert
+ *Return: abs(n)
+ */
+int _abs(int n)
+{
+	int a;
+
+	a = abs(n);
+
+	return (a);
+}
