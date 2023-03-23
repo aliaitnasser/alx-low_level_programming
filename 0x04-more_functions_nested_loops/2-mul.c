782090f44baf8936549a658a9db468b5491ac4b4
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-23 03:48:41 -0700

    mul

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ *mul - calculate the multiples of two integers
+ *@a: first integer argument
+ *@b: second integer argument
+ *Return: the multiple of a and b
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
