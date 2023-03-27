47b402e013f325505286b7fe7792af85d679b4c9
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-27 04:24:47 -0700

    puts

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,13 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+void _putchar(char c);
+
 void reset_to_98(int *n);
 
 void swap_int(int *a, int *b);
 
 int _strlen(char *s);
 
-void _put(char *str);
+void _puts(char *str);
 
 void print_rev(char *s);
 
