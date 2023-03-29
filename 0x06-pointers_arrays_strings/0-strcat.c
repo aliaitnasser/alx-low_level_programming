1ba97e26a564105b9f3a386524f29bebea6e6110
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-29 09:48:21 -0700

    compar two strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - Concatenates two strings together
- * @dest: the first string
- * @src: the second string
- * Return: return the concatenated string which is dest
+ * *_strcat - Concatenates two strings together
+ *@dest: the first string
+ *@src: the second string
+ *Return: return the concatenated string which is dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	i = 0;
-	while (dest[i] != '\n')
+	while (dest[i] != '\0')
 		i++;
-	dest[i] = ' ';
-	j = 0;
-	while (src[j] != '\n')
+
+	while (src[j] != '\0')
 	{
 		dest[i] = src[j];
 		i++;
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,29 +1,28 @@
-#include "main.h"
+#include "main.h"`
 
 /**
- * _strncat - concatenates n bytes from string dest to src
- * @dest: the string to concatenate to
- * @src: the string to concatenate from
- * @n: the number of bytes to get from src
- * Return: the final result in dest
+ *_strncat - concatenates n bytes from string dest to src
+ *@dest: the string to concatenate to
+ *@src: the string to concatenate from
+ *@n: the number of bytes to get from src
+ *Return: the final result in dest
  */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	int i;
-	int j;
+	int i = 0;
+	int j = 0;
 
-	i = 0;
-	j = 0;
-	while (dest[i])
+	while (dest[i] != '\0')
 		i++;
-	dest[i] = ' ';
-	while (src[j] && j < n)
+
+	while (src[j] != s[n])
 	{
 		dest[i] = src[j];
 		i++;
 		j++;
 	}
 	dest[i] = '\0';
+
 	return (dest);
 }
