c3552ecdfe6bbea499877a51009baa82bfb8b04d
Ali Ait Nasser <aliii.nassser@gamil.com>
2023-03-29 09:11:22 -0700

    concat strings

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,28 +1,27 @@
 #include "main.h"
 
 /**
- * _strcat - concatenate two strings
- * @dest: string to be appended to
- * @src: string to append
- * Return: concatenated string
+ * _strcat - Concatenates two strings together
+ * @dest: the first string
+ * @src: the second string
+ * Return: return the concatenated string which is dest
  */
 
 char *_strcat(char *dest, char *src)
 {
-        int i;
+	int i;
 	int j;
-	
-	i = 0;
-	while (dest[i])
-	   i++;
 
-	dest[i] = ' '; 
+	i = 0;
+	while (dest[i] != '\n')
+		i++;
+	dest[i] = ' ';
 	j = 0;
-	while (src[j])
+	while (src[j] != '\n')
 	{
-	   dest[i] = src[j];
-	   i++;
-	   j++;
+		dest[i] = src[j];
+		i++;
+		j++;
 	}
 	dest[i] = '\0';
 
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,11 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-int _putchar(char c);
+#include <stdio.h>
 
 char *_strcat(char *dest, char *src);
 
-char *_strncat(char *, char *src, int n);
+char *_strncat(char *dest, char *src, int n);
 
 char *_strncpy(char *dest, char *src, int n);
 
@@ -19,4 +19,12 @@ char *cap_string(char *);
 
 char *leet(char *);
 
-#endif
+char *rot13(char *);
+
+void print_number(int n);
+
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+
+void print_buffer(char *b, int size);
+
+#endif /* MAIN_H */
